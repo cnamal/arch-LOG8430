acd1449568d56bb58748d501806792f0ab2af681
Fabien BERQUEZ <fabienfd.berquez@gmail.com>
2016-01-28 19:19:09 +0100

    Implemented the Observer Design Pattern to allow the PlayerController to observe the PausablePlayer

--- src/main/java/com/namal/arch/controller/PausablePlayer.java
+++ src/main/java/com/namal/arch/controller/PausablePlayer.java
@@ -1,6 +1,7 @@
 package com.namal.arch.controller;
 
 import java.io.InputStream;
+import java.util.List;
 
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.AudioDevice;
@@ -11,13 +12,13 @@ import javazoom.jl.player.Player;
  * 
  * Source : http://stackoverflow.com/questions/12057214/jlayer-pause-and-resume-song
  *
+ * Adapted by Fabien Berquez to add the Observer Design Pattern and the stopped state.
+ * The constants describing the possible states of the PausablePlayer have also been moved to be shared
+ * by the events, the PlayerController and this class.
  */
 public class PausablePlayer {
 
-    private final static int NOTSTARTED = 0;
-    private final static int PLAYING = 1;
-    private final static int PAUSED = 2;
-    private final static int FINISHED = 3;
+	private List<PlayerController> observers;
 
     // the player actually doing all the work
     private final Player player;
@@ -26,7 +27,7 @@ public class PausablePlayer {
     private final Object playerLock = new Object();
 
     // status variable what player thread is doing/supposed to do
-    private int playerStatus = NOTSTARTED;
+    private int playerStatus = PausablePlayerEvent.NOTSTARTED;
 
     public PausablePlayer(final InputStream inputStream) throws JavaLayerException {
         this.player = new Player(inputStream);
@@ -35,14 +36,53 @@ public class PausablePlayer {
     public PausablePlayer(final InputStream inputStream, final AudioDevice audioDevice) throws JavaLayerException {
         this.player = new Player(inputStream, audioDevice);
     }
+    
+    /**
+     * Attach an observer to this player
+     */
+    public void attach(PlayerController playerController) {
+    	observers.add(playerController);
+    }
+    /**
+     * Detach the observer passed as a parameter
+     * @param playerController
+     */
+    public void detach(PlayerController playerController) {
+    	observers.remove(playerController);
+    }
+    
+    /**
+     * Notify each observer in the observers List. This method is called
+     * each time the status is changed. Other events might easily be implemented
+     * if needed.
+     * @param ev a PausablePlayerEvent
+     */
+    public void notifyObservers(PausablePlayerEvent ev) {
+    	for(PlayerController observer : observers) {
+    		observer.update(ev);
+    	}
+    }
 
     /**
-     * Starts playback (resumes if paused)
+     * Change the current status in this player. This method also generates
+     * a PausablePlayerEvent which encapsulates the data about the status changes
+     * and calls the notifyObservers method to notify all the observers attached
+     * to this object.
+     * @param newStatus
+     */
+    public void changeStatus(int newStatus) {
+    	playerStatus = newStatus;
+    	PausablePlayerEvent ev = new PausablePlayerEvent(PausablePlayerEvent.TYPE_STATECHANGED, newStatus);
+    	notifyObservers(ev);
+    }
+    
+    /**
+     * Starts playback (resumes if PausablePlayerEvent.PAUSED)
      */
     public void play() throws JavaLayerException {
         synchronized (playerLock) {
             switch (playerStatus) {
-                case NOTSTARTED:
+                case PausablePlayerEvent.NOTSTARTED:
                     final Runnable r = new Runnable() {
                         public void run() {
                             playInternal();
@@ -51,10 +91,10 @@ public class PausablePlayer {
                     final Thread t = new Thread(r);
                     t.setDaemon(true);
                     t.setPriority(Thread.MAX_PRIORITY);
-                    playerStatus = PLAYING;
+                    changeStatus(PausablePlayerEvent.PLAYING);
                     t.start();
                     break;
-                case PAUSED:
+                case PausablePlayerEvent.PAUSED:
                     resume();
                     break;
                 default:
@@ -64,42 +104,42 @@ public class PausablePlayer {
     }
 
     /**
-     * Pauses playback. Returns true if new state is PAUSED.
+     * Pauses playback. Returns true if new state is PausablePlayerEvent.PAUSED.
      */
     public boolean pause() {
         synchronized (playerLock) {
-            if (playerStatus == PLAYING) {
-                playerStatus = PAUSED;
+            if (playerStatus == PausablePlayerEvent.PLAYING) {
+                changeStatus(PausablePlayerEvent.PAUSED);
             }
-            return playerStatus == PAUSED;
+            return playerStatus == PausablePlayerEvent.PAUSED;
         }
     }
 
     /**
-     * Resumes playback. Returns true if the new state is PLAYING.
+     * Resumes playback. Returns true if the new state is PausablePlayerEvent.PLAYING.
      */
     public boolean resume() {
         synchronized (playerLock) {
-            if (playerStatus == PAUSED) {
-                playerStatus = PLAYING;
+            if (playerStatus == PausablePlayerEvent.PAUSED) {
+                changeStatus(PausablePlayerEvent.PLAYING);
                 playerLock.notifyAll();
             }
-            return playerStatus == PLAYING;
+            return playerStatus == PausablePlayerEvent.PLAYING;
         }
     }
 
     /**
-     * Stops playback. If not playing, does nothing
+     * Stops playback. If not PausablePlayerEvent.PLAYING, does nothing
      */
     public void stop() {
         synchronized (playerLock) {
-            playerStatus = FINISHED;
+            changeStatus(PausablePlayerEvent.STOPPED);
             playerLock.notifyAll();
         }
     }
 
     private void playInternal() {
-        while (playerStatus != FINISHED) {
+        while (playerStatus != PausablePlayerEvent.FINISHED && playerStatus != PausablePlayerEvent.STOPPED) {
             try {
                 if (!player.play(1)) {
                     break;
@@ -107,9 +147,9 @@ public class PausablePlayer {
             } catch (final JavaLayerException e) {
                 break;
             }
-            // check if paused or terminated
+            // check if PausablePlayerEvent.PAUSED or terminated
             synchronized (playerLock) {
-                while (playerStatus == PAUSED) {
+                while (playerStatus == PausablePlayerEvent.PAUSED) {
                     try {
                         playerLock.wait();
                     } catch (final InterruptedException e) {
@@ -127,7 +167,7 @@ public class PausablePlayer {
      */
     public void close() {
         synchronized (playerLock) {
-            playerStatus = FINISHED;
+            changeStatus(PausablePlayerEvent.FINISHED);
         }
         try {
             player.close();
