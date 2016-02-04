e59b644a3e44680f141bbbe40b837a0c42459bf9
Namal <chathura.namalgamuwa@ensimag.grenoble-inp.fr>
2016-02-03 23:19:56 -0500

    Fixed disconnect for Soundcloud

--- src/main/java/com/namal/arch/models/services/Soundcloud.java
+++ src/main/java/com/namal/arch/models/services/Soundcloud.java
@@ -332,7 +332,7 @@ public class Soundcloud implements AudioService, AudioServiceProvider,IAuthentif
 
 	@Override
 	public void disconnect() {
-		//FIXME NOT ONLY
-		isAuthenticated = false;		
+		isAuthenticated = false;
+		SharedPreferences.getPreferences().node("soundcloud").remove("oauth_token");
 	}
 }
