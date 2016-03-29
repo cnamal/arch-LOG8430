e9e57cbad27c8ff4977d37be8116aeb3ca0b4883
Fabien BERQUEZ <fabienfd.berquez@gmail.com>
2016-03-28 21:03:22 -0400

    Javadoc for controllers

--- TP1/src/main/java/com/namal/arch/controller/PausablePlayerEvent.java
+++ TP1/src/main/java/com/namal/arch/controller/PausablePlayerEvent.java
@@ -3,13 +3,32 @@ package com.namal.arch.controller;
 import com.namal.arch.utils.PlayerEventType;
 import com.namal.arch.utils.PlayerStatus;
 
+/**
+ * Class designed to encapsulate data from events occurring in the PausablePlayer.
+ * @author Fabien Berquez
+ *
+ */
 public class PausablePlayerEvent extends PlayerEvent {
+	/**
+	 * Attribute overriding the eventSource attribute of PlayerEvent.
+	 * The concrete type is PausablePlayer, as the source can only be a PausablePlayer instance.
+	 */
 	private PausablePlayer eventSource;
 	
+	/**
+	 * Constructor overriding the PlayerEvent constructor.
+	 * @param eventSource : a reference to the source of the event (a PausablePlayer instance)
+	 * @param eventType : the type of the event (values defined in the PlayerEventType enum) 
+	 * @param eventInformation : if relevant, complementary information about the event (the new status). Null otherwise.
+	 */
 	public PausablePlayerEvent(PausablePlayer eventSource, PlayerEventType eventType, PlayerStatus eventInformation) {
 		super(eventSource, eventType, eventInformation);
 	}
 	
+	/**
+	 * Gets the eventSource attribute value.
+	 * @return a reference to the event source object (a PausablePlayer instance).
+	 */
 	public PausablePlayer getEventSource() {
 		return eventSource;
 	}
--- TP1/src/main/java/com/namal/arch/utils/IPlayerObserver.java
+++ TP1/src/main/java/com/namal/arch/utils/IPlayerObserver.java
@@ -2,7 +2,15 @@ package com.namal.arch.utils;
 
 import com.namal.arch.controller.PlayerEvent;
 
+/**
+ * Interface to be implemented by the views observing the PlayerController.
+ * @author Fabien Berquez
+ *
+ */
 public interface IPlayerObserver {
-	
+	/**
+	 * Method receiving the events of the PlayerController to process them.
+	 * @param ev : a PlayerEvent encapsulating the data of the event that occurred in the PlayerController.
+	 */
 	public void update(PlayerEvent ev);
 }
