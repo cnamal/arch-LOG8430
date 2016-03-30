0d409cb57687b757bc697da56e186b4e46184a0b
Namal <chathura.namalgamuwa@ensimag.grenoble-inp.fr>
2016-03-30 14:39:59 -0400

    Farewell

---
+++ deleteFromZip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+zip -d Untitled.jar "config.properties"
+zip -d Untitled.jar "com/namal/arch/models/services/AudioServiceLoader.java"
+zip -d Untitled.jar "com/namal/arch/models/services/AudioServiceLoader.class"
