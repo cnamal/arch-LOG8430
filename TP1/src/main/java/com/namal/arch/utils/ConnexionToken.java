30224dbc506908c0b7ede9db81b609438a81c56a
Adrien Logut <adrien.logut@ensimag.grenoble-inp.fr>
2016-04-08 19:30:39 -0400

    Connexion token added

---
+++ TP1/src/main/java/com/namal/arch/utils/ConnexionToken.java
@@ -0,0 +1,26 @@
+package com.namal.arch.utils;
+
+/**
+ * Created by adrien on 16-04-08.
+ */
+public class ConnexionToken {
+
+    private static ConnexionToken instance;
+
+    private String token;
+
+    static public ConnexionToken getInstance(){
+        if(instance==null)
+            instance = new ConnexionToken();
+        return instance;
+    }
+
+    public String getToken(){
+        return token;
+    }
+
+    public void setToken(String token){
+        this.token = token;
+    }
+
+}
