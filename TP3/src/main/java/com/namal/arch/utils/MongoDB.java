aeaced830b486b91dd65fa9ac02f338d882486a5
Namal <chathura.namalgamuwa@ensimag.grenoble-inp.fr>
2016-04-10 19:24:46 -0400

    Readd MongoDB

---
+++ TP3/src/main/java/com/namal/arch/utils/MongoDB.java
@@ -0,0 +1,63 @@
+package com.namal.arch.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+import com.namal.arch.Main;
+
+/**
+ * MongoDB driver
+ * @author namalgac
+ *
+ */
+public class MongoDB {
+	
+	private static MongoClient mongoClient;
+	static MongoDatabase db;
+	
+	/**
+	 * Initialize the driver
+	 */
+	public static void init(){
+		Properties prop = new Properties();
+		String propFileName = "config.properties";
+
+		InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(propFileName);
+		if(inputStream==null){
+			System.err.println("config.properties wasn't found. Please create it and add necessary information using config.properties.dist .");
+			System.exit(1);
+		}
+			
+		try {
+			
+			prop.load(inputStream);
+			inputStream.close();
+			String uriString = "mongodb://";
+			if(prop.getProperty("dbuser")!=null){
+				uriString+=prop.getProperty("dbuser")+":"+prop.getProperty("dbpwd")+"@";
+			}
+			if(prop.getProperty("address")==null|| prop.getProperty("port")==null||prop.getProperty("db")==null){
+				System.err.println("For cross platform playlists to work, it needs a proper working mongodb database. Please follow the tutorial in the report.");
+				System.exit(1);
+			}else
+				uriString+=prop.getProperty("address")+":"+prop.getProperty("port")+"/"+prop.getProperty("db");
+			MongoClientURI uri = new MongoClientURI(uriString);
+			mongoClient = new MongoClient(uri);
+			db = mongoClient.getDatabase(uri.getDatabase());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @return the database
+	 */
+	public static MongoDatabase getDatabase(){
+		return db;
+	}
+}
